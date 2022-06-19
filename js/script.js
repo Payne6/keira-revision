c46a8780284950083a82bed8c30e7b699484c5a0
Payne6 <75501119+Payne6@users.noreply.github.com>
2022-06-19 21:35:03 +0100

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -89,7 +89,7 @@ re.save(q4);
 let q5 = new Question("Is science compatible with descriptions of creation found in the Quran?", "Many Muslims think so");
 q5.addChoice("No, all Muslims believe science and the Quran are incompatible");
 q5.addChoice("There are no descriptions of creation in the Quran");
-q5.addChoice("yes, but only science before the 20th century");
+q5.addChoice("Yes, but only science before the 20th century");
 re.save(q5);
 
 let q6 = new Question("Which two theories are used to support the Design Argument?", "The thumb theory and the watchmaker theory");
@@ -308,12 +308,121 @@ q44.addChoice("The celebration of guru Nanak's birthday");
 q44.addChoice("The custom of separating men and women in the temple");
 re.save(q44);
 
+let q45 = new Question("After Guru Nanak, how many other gurus were there?", "Ten");
+q45.addChoice("Nine");
+q45.addChoice("Eleven");
+q45.addChoice("Six");
+re.save(q45);
+
+let q46 = new Question("Guru Angad was the second Guru and wrote down Guru Nanak's teachings. What does he represent?", "Obedience");
+q46.addChoice("Equality");
+q46.addChoice("Purity");
+q46.addChoice("Tranquility");
+re.save(q46);
+
+let q47 = new Question("Which guru built the holy Sikh city of Amritsar (inluding the Golden Temple)?", "Guru Ram Das");
+q47.addChoice("Guru Nanak");
+q47.addChoice("Guru Gobind Singh");
+q47.addChoice("Guru Amar Das");
+re.save(q47);
+
+let q48 = new Question("How did Vyasa learn the Vedas?", "He was taught by Hindu gurus who lived in the forests");
+q48.addChoice("He was taught them by his parents, Parashara and Satyavati");
+q48.addChoice("He he found them in a field one day and took them home with him");
+q48.addChoice("When he was ten God recited the Vedas to him in a dream");
+re.save(q48);
+
+let q49 = new Question("How many parts did Vyasa split the Vedas into?", "Four");
+q49.addChoice("Seven");
+q49.addChoice("Ten");
+q49.addChoice("Twelve");
+re.save(q49);
+
+let q50 = new Question("It is believed that Vyasa wrote the Mahabharata", "True");
+q50.addChoice("False");
+re.save(q50);
+
+let q51 = new Question("Guru Purnima is", "A festival celebrating Vyasa's birthday");
+q51.addChoice("A festival celebrating Guru Nanak's birthday");
+q51.addChoice("A festival celebrating the construction of the Golden Temple");
+q51.addChoice("A festival celebrating the lives of the gurus");
+re.save(q51);
+
+let q52 = new Question("The meaning of dharma is", "Duty");
+q52.addChoice("Rebirth");
+q52.addChoice("Suffering");
+q52.addChoice("Purity");
+re.save(q52);
+
+let q53 = new Question("Moksha is", "Unity with Brahman");
+q53.addChoice("A son's duty to his family");
+q53.addChoice("The Hindu god of death");
+q53.addChoice("The soul");
+re.save(q53);
+
+let q54 = new Question("If God is personal then ??? is possible through ???", "A relationship, prayer");
+q54.addChoice("Punishment, divine justice");
+q54.addChoice("His forgiveness, love");
+q54.addChoice("Happiness, faith");
+re.save(q54);
+
+let q55 = new Question("If God is impersonal, God is seen as a kind of", "Force");
+q55.addChoice("Person");
+q55.addChoice("Belief");
+q55.addChoice("Problem");
+re.save(q55);
+
+let q56 = new Question("An immanent god is ??? and ???", "Present in the world, takes an active role in humanity");
+q56.addChoice("Separate from the world, doesn't interfere in human history");
+q56.addChoice("Present in the world, doesn't interfere with human history");
+q56.addChoice("Separate from the world, takes an active role in humanity");
+re.save(q56);
+
+let q57 = new Question("A transcendent god is ??? and ???", "Separate from the world, doesn't interfere in human history");
+q57.addChoice("Present in the world, takes an active role in humanity");
+q57.addChoice("Present in the world, doesn't interfere with human history");
+q57.addChoice("Separate from the world, takes an active role in humanity");
+re.save(q57);
+
+let q58 = new Question("Belief in more than one god is:", "Polytheism");
+q58.addChoice("Monotheism");
+re.save(q58);
+
+let q59 = new Question("Omnipotent means:", "All-powerful");
+q59.addChoice("All-knowing");
+q59.addChoice("Everywhere");
+re.save(q59);
+
+let q60 = new Question("Omniscient means:", "All-knowing");
+q60.addChoice("All-powerful");
+q60.addChoice("Everywhere");
+re.save(q60);
+
+let q61 = new Question("Omnipresent means:", "Everywhere");
+q61.addChoice("All-knowing");
+q61.addChoice("All-powerful");
+re.save(q61);
+
+let q62 = new Question("For many Christians, God the father is:", "The God of the Old Testament");
+q62.addChoice("The same as the Hindu god Brahman");
+q62.addChoice("The God of the Quran");
+q62.addChoice("Moses");
+re.save(q62);
+
+let q63 = new Question("Christians believe the Holy Spirit is:", "The presence of God in the world");
+q63.addChoice("The blood of Jesus");
+q63.addChoice("The voice of God");
+q63.addChoice("The light passed from guru to guru");
+re.save(q63);
+
+
 
 
 
 
 document.addEventListener("DOMContentLoaded", function() {
-    //Shuffles the questions and resets the question index
+    // VARIABLES
+    const mainBox = document.getElementById("questionContainer");
     const container = document.getElementById("questionBox");
     const nextButton = document.getElementById("nextQuestion");
     const startButton = document.getElementById("startButton");
@@ -321,11 +430,14 @@ document.addEventListener("DOMContentLoaded", function() {
     const progressMade = document.getElementsByClassName("progressMade")[0];
     var questionIndex = 0;
     var correct = 0;
-    let qs = re.questions;
+    var answered = false;
+    var answeredIncorrectly = [];
+    //Shuffles the questions
+    var qs = shuffleQuestions(re.questions, 15);
     let quizLength = qs.length;
 
-    shuffleArray(qs);
 
+    //FUNCTIONS
     function printQ() {
         //Displays questions and answers
         var newQuestion = qs[questionIndex];
@@ -345,11 +457,18 @@ document.addEventListener("DOMContentLoaded", function() {
                 answerPara.classList.add("correctChoice");
                 answerPara.addEventListener("click", function() {
                     this.classList.add("correctClicked");
+                    this.innerText = "Correct";
                 });
             } else {
                 answerPara.classList.add("incorrectChoice");
                 answerPara.addEventListener("click", function() {
                     this.classList.add("incorrectClicked");
+                    this.innerText = "Sorry, that's not right";
+                    for (q of re.questions) {
+                        if (q.question == questionPanelQuestion.innerText) {
+                            answeredIncorrectly.push(q);
+                        } 
+                    }
                 });
             }
             answerPara.innerText = ans;
@@ -372,15 +491,39 @@ document.addEventListener("DOMContentLoaded", function() {
         return percentAnswered;
     }
 
+    function printIncorrectAnswers(containerElement) {
+        containerElement.classList.add("incorrectContainer");
 
+        let heading = document.createElement("h3");
+        heading.innerText = "You answered the following questions incorrectly";
+        containerElement.append(heading);
+
+        var uniqueIncorrectAnswers = new Set(answeredIncorrectly);
+
+        for (q of uniqueIncorrectAnswers.values()) {
+            let incorrectAnsPara = document.createElement("p");
+            incorrectAnsPara.innerText = q.question;
+            containerElement.append(incorrectAnsPara);
+        }
+    }
+
+    function shuffleQuestions(questionsArray, numToAnswer) {
+        let questions = questionsArray;
+        shuffleArray(questions);
+        return questions.slice(0, numToAnswer);
+    }
+
+
+    // EVENT LISTENERS
     startButton.addEventListener("click", function() {
         clearContainer(container);
         printQ();
         progressBar.style.display = "block";
         nextButton.style.display = "block";
-    })
+    });
 
     nextButton.addEventListener("click", function() {
+        answered = false;
         questionIndex ++;
         clearContainer(container);
         if (questionIndex < qs.length) {
@@ -388,6 +531,9 @@ document.addEventListener("DOMContentLoaded", function() {
             progressMade.style.width = `${calculateProgress(questionIndex, quizLength)}%`;
         } else {
             alert("There are no more questions!\n\nRefresh the page to take the test again.");
+            clearContainer(mainBox);
+            printIncorrectAnswers(mainBox);
+            progressMade.style.width = `${calculateProgress(questionIndex, quizLength)}%`;
         }
     });
 
